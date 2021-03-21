4098470a21737e85c11c93799679c1b07ca669eb
Diane998 <gutierrez.almiradyan@gmail.com>
2021-03-21 10:44:06 +0800

    implemented reselect, replaced user selectors with memoized selectors

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
+import { selectCurrentUser } from '../redux/user/userSelectors';
 import { setCurrentUser } from '../redux/user/userActions';
 import { fetchCollections } from '../redux/shop/shopActions';
 
 import App from '../components/App';
 
-const mapStateToProps = ({ user: { currentUser } }) => ({ currentUser });
+const mapStateToProps = state => ({ currentUser: selectCurrentUser(state) });
 
 export default connect(mapStateToProps, { fetchCollections, setCurrentUser })(
   App
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux';
+import { selectCurrentUser } from '../redux/user/userSelectors';
 import { setTabIndex } from '../redux/tabs/tabsActions';
 import Header from '../components/header/Header';
 
-const mapStateToProps = ({ user: { currentUser } }) => ({ currentUser });
+const mapStateToProps = state => ({ currentUser: selectCurrentUser(state) });
 
 export default connect(mapStateToProps, { setTabIndex })(Header);
