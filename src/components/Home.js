fed37a93a76aa04e9a00ceb51b2bc1b86ecb730a
Diane998 <gutierrez.almiradyan@gmail.com>
2021-03-27 13:19:38 +0800

    Extracted styles to makeStyles hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,47 @@ import Carousel from 'react-material-ui-carousel';
 import CollectionCardContainer from '../containers/CollectionCardContainer';
 
 const useStyles = makeStyles(theme => ({
+  homeContainer: { width: '100vw' },
+  textContainer: {
+    margin: '6em 0',
+    width: '50vw',
+    [theme.breakpoints.down('md')]: {
+      width: '90vw'
+    }
+  },
+  title: { marginBottom: '1em' },
+  descriptiion: { lineHeight: '1.7em' },
+  collectionsContainer: {
+    width: '80vw',
+    [theme.breakpoints.down('md')]: {
+      width: '100vw'
+    }
+  },
+  collectionContainer: {
+    margin: '1em',
+    [theme.breakpoints.down('sm')]: {
+      margin: '1.5em 0'
+    }
+  },
+  carouselContainer: { width: '100vw' },
+  carouselContent: {
+    width: '100vw',
+    height: '85vh',
+    [theme.breakpoints.down('md')]: {
+      height: '35vh'
+    },
+    [theme.breakpoints.down('sm')]: {
+      height: '60vh'
+    }
+  },
+  carouselActions: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: '2em'
+  },
   button: {
     ...theme.button,
     fontSize: '1.1em',
-    '&:hover': {
-      backgroundColor: theme.palette.common.crimson
-    }
+    ...theme.buttonFillAnimation
   }
 }));
 
@@ -23,11 +58,9 @@ const Banner = ({ collection, history }) => {
 
   return (
     <CardMedia
-      style={{
-        width: '100vw'
-      }}
       image={collection.collectionPage.imageUrl}
       title={collection.Name}
+      className={classes.carouselContainer}
     >
       <Grid
         item
@@ -35,14 +68,9 @@ const Banner = ({ collection, history }) => {
         direction='column'
         alignItems='flex-end'
         justify={matchesSM ? 'flex-end' : 'center'}
-        style={{ width: '100vw', height: matchesSM ? '50vh' : '85vh' }}
+        className={classes.carouselContent}
       >
-        <div
-          style={{
-            padding: matchesSM ? '1em' : '2em',
-            backgroundColor: 'rgba(0,0,0,0.5)'
-          }}
-        >
+        <div className={classes.carouselActions}>
           <Grid item>
             <Typography
               variant={matchesMD ? 'h3' : 'h1'}
@@ -72,60 +100,45 @@ const Banner = ({ collection, history }) => {
 };
 
 const Home = ({ collections, history }) => {
+  const classes = useStyles();
   const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
   const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
 
-  return collections ? (
-    <Grid container justify='center' style={{ width: '100vw' }}>
-      <Grid
-        item
-        container
-        align={matchesMD ? 'center' : ''}
-        spacing={4}
-        direction={matchesMD ? 'column' : 'row'}
-        style={{ margin: '0 0 6em 0' }}
-      >
-        <Grid item style={{ padding: 0 }}>
-          <Carousel
-            autoPlay={true}
-            animation={'fade'}
-            indicators={true}
-            timeout={1000}
-            interval={5000}
-            navButtonsAlwaysVisible={false}
-          >
-            {collections.map((c, i) => (
-              <Banner collection={c} key={i} history={history} />
-            ))}
-          </Carousel>
-        </Grid>
+  return collections.length ? (
+    <Grid container justify='center' className={classes.homeContainer}>
+      <Grid item style={{ padding: 0 }}>
+        <Carousel
+          autoPlay={true}
+          animation={'fade'}
+          indicators={true}
+          timeout={1000}
+          interval={5000}
+          navButtonsAlwaysVisible={false}
+        >
+          {collections.map((c, i) => (
+            <Banner collection={c} key={i} history={history} />
+          ))}
+        </Carousel>
       </Grid>
       <Grid
         item
         container
-        style={{
-          margin: '1em 0 5em 0',
-          width: matchesMD ? '90vw' : '50vw'
-        }}
+        direction='column'
+        jusify='center'
+        alignItems
+        className={classes.textContainer}
       >
         <Typography
           variant={matchesMD ? 'h3' : 'h2'}
-          style={{
-            color: 'black',
-            background: '#ffffff82',
-            boxShadow: '5px 0 0 #ffffff82',
-            margin: matchesMD ? '1em 0 0.3em 0' : '0 auto'
-          }}
+          align='center'
+          className={classes.title}
         >
           UNASHAMED TO BE DIFFERENT
         </Typography>
         <Typography
-          align={matchesMD ? 'left' : 'center'}
           variant='body2'
-          style={{
-            lineHeight: '1.7em'
-          }}
+          align='center'
+          className={classes.description}
         >
           Unashamed to be different and taking strength from size, we’ve carved
           a new timeline in the watch market since our arrival in 2005. Coming
@@ -137,14 +150,10 @@ const Home = ({ collections, history }) => {
         item
         container
         justify='center'
-        style={{ width: matchesMD ? '100vw' : '80vw' }}
+        className={classes.collectionsContainer}
       >
         {collections.map((c, i) => (
-          <Grid
-            item
-            key={i}
-            style={{ margin: matchesSM ? '1em 0.5em' : '1em 1em' }}
-          >
+          <Grid item key={i} className={classes.collectionContainer}>
             <CollectionCardContainer collection={c} />
           </Grid>
         ))}
